c35875947842170be60644e7be2b23ab40ac0363
Vít Ružička <74555761+vito104@users.noreply.github.com>
2022-06-18 18:11:00 +0200

    Add files via upload

---
+++ hlavicka.php
@@ -0,0 +1,17 @@
+        <nav>
+            <ul class="flex-box">
+                <li>
+                    <a href="index.php">Hlavní Stránka</a>
+                </li>
+                <li>
+                    <a href="http://frantapastika.ruzicka-markojugend.ga">WordPress</a>
+                </li>
+                <li>
+                    <a href="galerie.php">Galerie</a>
+                </li>
+                <li>
+                    <a>Zpětná vazba</a>
+                </li>
+                <li><a onclick="darkmode()">Dark Mode</a></li>
+            </ul>
+        </nav>
