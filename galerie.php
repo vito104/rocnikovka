69502b2a73cde9f9d039541aa9dc19b6fe7068b1
Vít Ružička <74555761+vito104@users.noreply.github.com>
2022-06-19 02:57:53 +0200

    Add files via upload

---
+++ galerie.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="galerie.css">
+    <script src="main.js"></script>
+    <script src="galerie.js"></script>
+    <title>Document</title>
+</head>
+<body>
+
+<!-- hlavička -->
+  <header>
+    <?php 
+    include "hlavicka.php"
+    ?>
+  </header>  
+<!-- galerie -->
+
+  <div class="galerie">
+        <div class="galerka"><a href="https://image.pmgstatic.com/files/images/film/photos/159/871/159871072_50c6fa.jpg" data-lightbox="close" data-title="Linkedin"><img src="https://image.pmgstatic.com/files/images/film/photos/159/871/159871072_50c6fa.jpg"></a></div>
+        <div class="galerka"><a href="http://databazeznamychmist.cz/wp-content/uploads/2018/07/sFA.jpg" data-lightbox="close" data-title="Facebook"><img src="http://databazeznamychmist.cz/wp-content/uploads/2018/07/sFA.jpg"></a></div>
+        <div class="galerka"><a href="https://photos.libimseti.cz/img/081126/1/iqPF_4xRDJ.jpg" data-lightbox="close" data-title="Instagram"><img src="https://photos.libimseti.cz/img/081126/1/iqPF_4xRDJ.jpg"></a></div>
+        <div class="galerka"><a href="http://databazeznamychmist.cz/wp-content/uploads/2018/07/iqPF_4xRDi.jpg" data-lightbox="close" data-title="Tinder"><img src="http://databazeznamychmist.cz/wp-content/uploads/2018/07/iqPF_4xRDi.jpg"></a></div>
+        <div class="galerka"><a href="https://i.ytimg.com/vi/aL9d6kAl82c/hqdefault.jpg" data-lightbox="close" data-title="To niiiiiic"><img src="https://i.ytimg.com/vi/aL9d6kAl82c/hqdefault.jpg"></a></div>
+    </div>
+   
+
+
+
+</body>
+</html>
